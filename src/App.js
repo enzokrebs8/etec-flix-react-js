bb6c2b789da29351901cd1369193b22019f68c50
enzokrebs8 <enzokrebs8@gmail.com>
2024-08-01 11:10:33 -0300

    Primeira versão

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <body>
+        <h1>ETEC MCM</h1>
+        <p>Olá mundo!</p>
+      </body>
+    </>
+  );
+}
+
+export default App;
